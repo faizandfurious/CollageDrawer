94d9a3df922d861efdbd9369c7fcbecd15e74e93
Faiz <fabbasi@andrew.cmu.edu>
2012-10-03 18:27:10 -0400

    Completed

--- src/ssui/fabbasi/collagedrawer/OvalClip.java
+++ src/ssui/fabbasi/collagedrawer/OvalClip.java
@@ -10,15 +10,18 @@ public class OvalClip extends ArtistBase {
 	Path path;
 	Canvas onCanvas;
 	public OvalClip(float x, float y, float w, float h) {
+		this.setPosition(x, y);
+		this.setSize(w, h);
 		oval = new RectF();
-		oval.set(x, y, w, h);
+		oval.set(0, 0, w, h);
 		path = new Path();
 		path.addOval(oval, Path.Direction.CW);
 		
 	}
 	
 	@Override
-	public void draw(Canvas onCanvas){
+	public void draw(Canvas onCanvas) {
+		
 		//Checks to see if the current Artist has children. If so, iterate through the children and set their position.
 		if(children.size() > 0){
 			//Draw children
